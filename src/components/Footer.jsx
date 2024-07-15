63a4ee1e2b5c268c40d1c5839095077f58493e62
Robdsjr007 <robson.santana15@etec.sp.gov.br.com>
2024-07-15 19:20:53 -0300

    terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <h3>Escreva sobre o que você tem interesse!</h3>
-        <p>Desenvolvido por &copy; <a href='' target='blank'>Robdsjr</a></p>
+        <p>Desenvolvido por &copy;<a href='https://www.linkedin.com/in/robdsjr007/' target='blank'>Robdsjr</a></p>
     </footer>
   )
 }
